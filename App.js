aae7565bebf27f7cb25ae259bbd4001e86a6c353
Abdullah <abdullah.celik1409@gmail.com>
2026-07-06 02:27:38 +0300

    SafeAreaProvider eklendi - web uyumlulugu duzeltildi

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="dark" />
-      <HomeScreen />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="dark" />
+        <HomeScreen />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
